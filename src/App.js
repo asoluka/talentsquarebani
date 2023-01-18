395766d3c0ef3d606439d26203a47d597a027c9f
Austin Asoluka <stracool9@gmail.com>
2023-01-18 20:08:45 +0100

    add relevant pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Login, NotFound } from "./components";
+import { ErrorPage } from "./components/pages/ErrorPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Login />} errorElement={<ErrorPage />}>
+      <Route exact path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
